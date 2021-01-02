9497f4837398a189949f891bcd4cbec9a6dc790f
Vikash Vishwakarma <vishwakarma.vikash1601@gmail.com>
2021-01-02 12:04:20 +0530

    Add files via upload

---
+++ precache-manifest.abc19384e645c3b26ad66291bc9e638c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdb0d91d203e4f95b1dad386f823d6ff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1198decdc1c5ccde793a",
+    "url": "/static/css/2.9a9b1a2c.chunk.css"
+  },
+  {
+    "revision": "0a371ad4aa005d3e1283",
+    "url": "/static/css/main.0f6cd66b.chunk.css"
+  },
+  {
+    "revision": "1198decdc1c5ccde793a",
+    "url": "/static/js/2.5e3c2a46.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.5e3c2a46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a371ad4aa005d3e1283",
+    "url": "/static/js/main.04c78b3f.chunk.js"
+  },
+  {
+    "revision": "f971b67155acae96f770",
+    "url": "/static/js/runtime-main.fd1456fd.js"
+  },
+  {
+    "revision": "82d2a04ed2bf263c89bd759a63c850e3",
+    "url": "/static/media/background.82d2a04e.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
